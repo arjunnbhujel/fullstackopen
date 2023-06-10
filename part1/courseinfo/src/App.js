1551e25317dbc0cc5c99a21e0b3a5ac8655502cc
arjunnbhujel <arjunbhujel600@gmail.com>
2023-06-10 13:34:43 +0545

    1.4: Refactoring of Code without use of array loop

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -15,21 +15,22 @@ const Header = (props) => {
 // Part is called in Content Component which further accepts array of parts in App Component
 
 const Content = ( props ) => {
+  console.log( props.parts[0].name )
     return (
       <div>
         <>
-          <h2>{props.part1.name}</h2>
-          <p>The Number of Exercises are = {props.part1.exercises}</p>
+          <h2>{props.parts[0].name}</h2>
+          <p>The Number of Exercises are = {props.parts[0].exercises}</p>
         </>
 
         <>
-          <h2>{props.part2.name}</h2>
-          <p>The Number of Exercises are = {props.part2.exercises}</p>
+          <h2>{props.parts[1].name}</h2>
+          <p>The Number of Exercises are = {props.parts[1].exercises}</p>
         </>
 
         <>
-          <h2>{props.part3.name}</h2>
-          <p>The Number of Exercises are = {props.part3.exercises}</p>
+          <h2>{props.parts[2].name}</h2>
+          <p>The Number of Exercises are = {props.parts[2].exercises}</p>
         </>
 
       </div>
@@ -37,8 +38,9 @@ const Content = ( props ) => {
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
-    <p>There are total of {props.exercises1 + props.exercises2 + props.exercises3} No of exercises.</p>
+    <p>There are total of {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} No of exercises.</p>
   )
 }
 
@@ -47,25 +49,28 @@ const App = () => {
   const course = 'Half Stack application development'
 
   // 1.4 : refactoring the code with the help of below variable
+  // There is no use of any array loop as instructed in the course
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return(
     <div>
       <Header course={course} />
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
