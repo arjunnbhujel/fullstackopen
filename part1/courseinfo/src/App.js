dec52eedfe904ce68c8a2f90e8853dc3ce16b3c7
arjunnbhujel <arjunbhujel600@gmail.com>
2023-06-10 13:18:31 +0545

    1.4 Refactoring the code to work with the variable

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,22 +4,8 @@
 
 // Exercise 1.3: Printing props to the Console.
 const Header = (props) => {
-  // Printing props to Console
-  console.log(props)
   return (
     <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  // Printing props to Console
-  console.log(props) //
-  return (
-    <div>
-      <h2>{props.part}</h2>
-      <p>The number of exercises are = {props.exercise}</p>
-      <br />
-    </div>
   )
 }
 
@@ -28,22 +14,29 @@ const Part = (props) => {
 // Created another component i.e. Part.
 // Part is called in Content Component which further accepts array of parts in App Component
 
-const Content = (props) => {
-  // Printing props to Console
-    console.log(props)
-    // use of array.map Prototype
-    // index === parts[i] where i increment by 1 untill the end of array
+const Content = ( props ) => {
     return (
       <div>
-        {props.parts.map( (index) => (<Part part = {index.name} exercise = {index.exercises}/>)
-        )}
+        <>
+          <h2>{props.part1.name}</h2>
+          <p>The Number of Exercises are = {props.part1.exercises}</p>
+        </>
+
+        <>
+          <h2>{props.part2.name}</h2>
+          <p>The Number of Exercises are = {props.part2.exercises}</p>
+        </>
+
+        <>
+          <h2>{props.part3.name}</h2>
+          <p>The Number of Exercises are = {props.part3.exercises}</p>
+        </>
+
       </div>
     )
 }
 
 const Total = (props) => {
-  // Printing props to Console
-  console.log(props) //
   return (
     <p>There are total of {props.exercises1 + props.exercises2 + props.exercises3} No of exercises.</p>
   )
@@ -53,17 +46,26 @@ const Total = (props) => {
 const App = () => {
   const course = 'Half Stack application development'
 
-  const parts = [
-    { name : 'Fundamentals of React', exercises : 10},
-    { name : 'Using props to pass data', exercises : 7},
-    { name : 'State of a component', exercises : 14}
-  ]
+  // 1.4 : refactoring the code with the help of below variable
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return(
     <div>
       <Header course={course} />
-      <Content parts = {parts} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
